e84e4472b8f4252b555e13a37fde9630643289db
AvaveryU <juliya.belka.19@yandex.ru>
2023-01-03 00:18:03 +0300

    models

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import path from 'path';
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+
+const { PORT = 3000, BASE_PATH } = process.env;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+//app.use('/users', usersRouter);
+//app.use('/cards', cardsRouter);
+app.use((req: any, res: Response, next) => {
+  req.user = {
+    _id: '63b2c9cf97c52473a263f217'
+  };
+  next();
+});
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => {
+  console.log(BASE_PATH);
+});
---
+++ src/models/user.ts
@@ -0,0 +1,28 @@
+import { model, Schema } from 'mongoose';
+
+export interface IUser {
+  name: string; //имя пользователя
+  about: string; //информация о пользователе
+  avatar: string; //ссылка на аватарку
+}
+
+const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  about: {
+    type: String,
+    minlength: 2,
+    maxlength: 200,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
+
+export default model<IUser>('user', userSchema);
